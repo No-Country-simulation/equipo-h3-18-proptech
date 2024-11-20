6040eaf4c5930249dd17a480e70efb98aaaf1b20
Deivison Jimenez <deivi.jimenez@gmail.com>
2024-11-19 21:53:30 -0400

    backend/create/add contract GenericRepository

--- Backend/h3-18-proptechback/h3-18-proptechback.Identity/Services/AuthServices.cs
+++ Backend/h3-18-proptechback/h3-18-proptechback.Identity/Services/AuthServices.cs
@@ -1,6 +1,7 @@
 ﻿using h3_18_proptechback.Application.Contracts.Identity;
 using h3_18_proptechback.Application.Models.Identity;
 using h3_18_proptechback.Identity.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 
 
@@ -19,14 +20,39 @@ namespace h3_18_proptechback.Identity.Services
             _jwtSettings = jwtSettings;
         }
 
-        public Task<AuthReponse> Login(AuthRequest request)
+        public async Task<AuthReponse> Login(AuthRequest request)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            if (user == null) 
+            {
+                throw new Exception($"E usuario con email {request.Email} no existe");
+            }
+            
+            var resultado = await _signInManager.PasswordSignInAsync(user.UserName, request.Password, false, lockoutOnFailure: false);
+            if(!resultado.Succeeded) 
+            {
+                throw new Exception($"Las credenciales son incorrectas");
+            }
+
+            var authResponse = new AuthReponse
+            {
+                Id = user.Id,
+                Token = "",
+                Email = user.Email,
+                UserName = user.UserName,
+
+            };
+            return authResponse;
         }
 
-        public Task<RegistrationResponse> Register(RegistrationRequest request)
-        {
-            throw new NotImplementedException();
-        }
+            public async Task<RegistrationResponse> Register(RegistrationRequest request)
+            {
+                var existingUser = await _userManager.FindByNameAsync(request.Username);
+
+                throw new NotImplementedException();
+            
+
+            }
     }
 }
