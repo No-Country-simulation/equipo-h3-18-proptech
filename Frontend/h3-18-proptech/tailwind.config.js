8181a34e1bd5e4a60c7a1cf5018739a4b628e79a
Angel Añez <angdavid2910@gmail.com>
2024-11-22 18:43:15 -0400

    front: refactor: add color palette

--- Frontend/h3-18-proptech/tailwind.config.js
+++ Frontend/h3-18-proptech/tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3D5A80",
+        primaryVar1: "#5D7595",
+        primaryVar2: "#334B6B",
+        secondary: "#EC8B57",
+        secondaryVar1: "#E77C44",
+        tertiary: "#BEC8D5",
+        constrast: "#FFFFFF",
+        base: "#1E1E1E",
+        background: "#F8F8F8",
+        disabled: "#A0A0A0",
+        success: "#2ECC71",
+        error: "#C1121F"
+      },
+      fontFamily: {
+        nunito: ["nunito", "sans-serif"],
+        lato: ["lato", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 };
