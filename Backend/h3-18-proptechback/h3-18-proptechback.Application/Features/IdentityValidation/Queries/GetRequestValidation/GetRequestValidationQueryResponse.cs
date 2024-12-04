edd579a40968f166cb190b4c97923594ac5d15e7
abelmv <mvabel23@gmail.com>
2024-12-04 14:33:04 -0300

    back: feature(get-requests) add service for get all pending request identity validation

---
+++ Backend/h3-18-proptechback/h3-18-proptechback.Application/Features/IdentityValidation/Queries/GetRequestValidation/GetRequestValidationQueryResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3_18_proptechback.Application.Features.IdentityValidation.Queries.GetRequestValidation
+{
+    public class GetRequestValidationQueryResponse
+    {
+        public Guid IdRequest { get; set; }
+        public string FullName { get; set; }
+        public string Role {  get; set; }
+    }
+}
---
+++ Backend/h3-18-proptechback/h3-18-proptechback.Application/Features/IdentityValidation/Queries/ValidateIdentityQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using h3_18_proptechback.Application.Contracts.Identity;
+using h3_18_proptechback.Application.Contracts.Persistence.DataUsers;
+using h3_18_proptechback.Application.Features.IdentityValidation.Queries.GetRequestValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3_18_proptechback.Application.Features.IdentityValidation.Queries
+{
+    internal class ValidateIdentityQueryHandler
+    {
+        private readonly IDataUserRepository _dataUserRepository;
+        private readonly IUserIdentityService _userIdentityService;
+
+        public ValidateIdentityQueryHandler(IDataUserRepository dataUserRepository,
+            IUserIdentityService userIdentityService)
+        {
+            _dataUserRepository = dataUserRepository;
+            _userIdentityService = userIdentityService;
+        }
+
+        public async Task<List<GetRequestValidationQueryResponse>> GetPendingRequest()
+        {
+            var dataUsers = _dataUserRepository.GetDataUserPending().ToList();
+            dataUsers.Select(async d =>
+            {
+                var appUserForDataUser = await _userIdentityService.GetByIdIdentityUser(d.Createby!);
+                return new GetRequestValidationQueryResponse()
+                {
+                    IdRequest = d.ID,
+                    FullName = string.Concat(appUserForDataUser.Name, " ", appUserForDataUser.LastName),
+                    Role = "hola"
+                };
+            }).ToList();
+        }
+    }
+}
