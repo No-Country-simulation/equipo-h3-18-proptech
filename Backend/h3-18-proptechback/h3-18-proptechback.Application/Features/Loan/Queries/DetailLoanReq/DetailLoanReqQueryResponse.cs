6d72308692dd544b4641989715c48a407387e244
abelmv <mvabel23@gmail.com>
2024-12-05 23:57:08 -0300

    back: feature(get-details-request-loan) add model and scorecredit for detail request loan

---
+++ Backend/h3-18-proptechback/h3-18-proptechback.Application/Features/Loan/Queries/DetailLoanReq/DetailLoanReqQueryResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace h3_18_proptechback.Application.Features.Loan.Queries.DetailLoanReq
+{
+    public sealed class DetailLoanReqBase
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string DNI { get; set; }
+        public string CUIT { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int CreditScore { get; set; }
+        public string SalaryURL { get; set; }
+        public string Salary2URL { get; set; }
+        public string Salary3URL { get; set; }
+        public string ProofOfAddressURL { get; set; }
+    }
+    public class DetailLoanReqQueryResponse
+    {
+        public string LotCost { get; set; }
+        public string DownPayment { get; set; }
+        public string QuotasCount { get; set; }
+        
+    }
+}
