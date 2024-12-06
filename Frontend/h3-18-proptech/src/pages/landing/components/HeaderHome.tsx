aa03188eb9e3854f6ab895dd6734425089e9dacd
Angel Añez <angdavid2910@gmail.com>
2024-12-05 22:37:22 -0400

    front: style(landing): add carousel images

--- Frontend/h3-18-proptech/src/pages/landing/components/HeaderHome.tsx
+++ Frontend/h3-18-proptech/src/pages/landing/components/HeaderHome.tsx
@@ -3,7 +3,7 @@ import { SwitchButton } from "../../../components/common";
 export const HeaderHome = () => {
   return (
     <div className="py-4 md:py-10 bg-tertiary justify-center flex items-center">
-      <p className=" hidden md:block text-title-large-regular max-w-[340px]">
+      <p className=" hidden md:block text-title-large-semi-bold max-w-[340px]">
         Elige tu perfil y accede a la información que se adapte a tus
         necesidades.
       </p>
