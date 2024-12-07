5260cc89a734bd5386a51b6cf2f966e56e153206
Deivison Jimenez <deivi.jimenez@gmail.com>
2024-12-07 14:32:14 -0400

    backend/create/add finantial calculator

---
+++ Backend/h3-18-proptechback/h3-18-proptechback.Application/Features/Calculators/InvestmantCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3_18_proptechback.Application.Features.Calculators
+{
+    public class InvestmantCalculator
+    {
+        public static class InvestmentInterest 
+        {
+            public const double interest = 0.01531;
+        }
+
+        private readonly decimal _initialInvestment;
+        private readonly double _interest;
+
+
+        public InvestmantCalculator(decimal initialInvestment, double interest, int months)
+        {
+            _initialInvestment = initialInvestment;
+            _interest = InvestmentInterest.interest;
+
+        }
+
+    }
+}
