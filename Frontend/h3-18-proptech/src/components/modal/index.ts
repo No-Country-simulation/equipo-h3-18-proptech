6167f8a0ae5dd3c1eb4ba0998ce355ff7261fb72
Angel Añez <angdavid2910@gmail.com>
2024-12-08 16:48:33 -0400

    front: feat(investor): add invest and retire money modals

--- Frontend/h3-18-proptech/src/components/modal/index.ts
+++ Frontend/h3-18-proptech/src/components/modal/index.ts
@@ -1,1 +1,3 @@
 export * from "./Modal"
+export * from "./InvestModal"
+export * from "./RetireMoneyModal"
--- Frontend/h3-18-proptech/src/pages/private/investor/DashboardInvestorPage.tsx
+++ Frontend/h3-18-proptech/src/pages/private/investor/DashboardInvestorPage.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { BigInfoCard, Button, InfoCard } from "../../../components/common";
 import { CalendarIcon, CashIcon, GraphIcon } from "../../../components/icons";
-import InvestorGraph from "./components/InvestorGraph";
+import { InvestModal, RetireMoneyModal } from "../../../components/modal";
+import { InvestorGraph } from "./components";
 
-interface InvestInfo {
+export interface InvestInfo {
   montoActual: number;
   gananciaTotal: number;
   mesesInv: number;
@@ -86,81 +87,115 @@ const fakeData = [
 ];
 
 export function DashboardInvestorPage() {
-  const actualYear = new Date().getFullYear()
-  const [investInfo, setInvestInfo] = useState<InvestInfo>({
+  const actualYear = new Date().getFullYear();
+  const [investInfo, setInvestInfo] = useState<InvestInfo | undefined>({
     montoActual: 0,
     gananciasPorMes: [{ monto: 0, mes: 0 }],
     year: 2000,
     gananciaTotal: 0,
     mesesInv: 0,
-    startYear: 2000
+    startYear: 2000,
   });
 
   const [selectedYear, setSelectedYear] = useState(actualYear);
+  const [openInvestModal, setOpenInvestModal] = useState(false);
+  const [openRetireModal, setOpenRetireModal] = useState(false);
+
+  useEffect(() => {
+    const response = fakeData.find((data) => data.year === actualYear);
+    if (response) {
+      setInvestInfo(response);
+    }
+  }, []);
 
   useEffect(() => {
     const response = fakeData.find((data) => data.year === selectedYear);
     if (response) {
-      setInvestInfo(response);
+      setInvestInfo(
+        (state) =>
+          state && {
+            ...state,
+            gananciasPorMes: response.gananciasPorMes,
+            year: response.year,
+          }
+      );
     } else {
-      setInvestInfo((state) => ({ ...state, gananciasPorMes: [] }));
+      setInvestInfo((state) => state && { ...state, gananciasPorMes: [] });
     }
   }, [selectedYear]);
 
   return (
-    <div className="bg-background flex-1 flex flex-col items-center">
-      <div className="flex flex-col md:flex-row gap-4 justify-between items-center md:w-[1050px] my-8">
-        <h2 className="text-headline-small-medium">Resumen Financiero</h2>
+    <>
+      <div className="bg-background flex-1 flex flex-col items-center">
+        <div className="flex flex-col md:flex-row gap-4 justify-between items-center md:max-w-[1050px] my-8">
+          <h2 className="text-headline-small-medium">Resumen Financiero</h2>
+        </div>
+        {investInfo ? (
+          <>
+            <div className="flex flex-col md:flex-row justify-between gap-3 pb-6 md:pb-16">
+              <InfoCard
+                title="Monto actual"
+                icon={<CashIcon />}
+                value={`$${investInfo.montoActual}`}
+              />
+              <InfoCard
+                title="Ganancia total"
+                icon={<GraphIcon />}
+                value={`$${investInfo.gananciaTotal}`}
+              />
+              <InfoCard
+                title="Meses invirtiendo"
+                icon={<CalendarIcon />}
+                value={investInfo.mesesInv.toString()}
+              />
+            </div>
+            <InvestorGraph
+              year={selectedYear}
+              setYear={setSelectedYear}
+              gananciasPorMes={investInfo.gananciasPorMes}
+              startYear={investInfo.startYear}
+            />
+            <div className="flex flex-col md:flex-row my-20 gap-6 justify-start md:max-w-[1050px] ">
+              <Button
+                color="primary-orange"
+                size="medium"
+                type="button"
+                onClick={() => setOpenInvestModal(true)}
+              >
+                Invertir
+              </Button>
+              <Button
+                color="secondary"
+                size="medium"
+                type="button"
+                onClick={() => setOpenRetireModal(true)}
+              >
+                Retirar dinero
+              </Button>
+            </div>
+          </>
+        ) : (
+          <BigInfoCard
+            buttonText="Invertir"
+            icon={<img src="assets/icono-inversor.png" />}
+            to="/investor"
+          >
+            Actualmente no tienes inversiones activas registradas en tu cuenta.
+          </BigInfoCard>
+        )}
       </div>
-      {investInfo ? (
-        <>
-          <div className="flex flex-col md:flex-row justify-between gap-3 pb-6 md:pb-16">
-            <InfoCard
-              title="Monto actual"
-              icon={<CashIcon />}
-              value={`$${investInfo.montoActual}`}
-            />
-            <InfoCard
-              title="Ganancia total"
-              icon={<GraphIcon />}
-              value={`$${investInfo.gananciaTotal}`}
-            />
-            <InfoCard
-              title="Meses invirtiendo"
-              icon={<CalendarIcon />}
-              value={investInfo.mesesInv.toString()}
-            />
-          </div>
-          <InvestorGraph
-            year={selectedYear}
-            setYear={setSelectedYear}
-            gananciasPorMes={investInfo.gananciasPorMes}
-            startYear={investInfo.startYear}
-          />
-          <div className="flex flex-col md:flex-row my-20 gap-6 justify-start md:w-[1050px] ">
-            <Button
-              color="primary-orange"
-              size="medium"
-              type="link"
-              to="/investor"
-            >
-              Invertir
-            </Button>
-            <Button color="secondary" size="medium" type="link" to="/investor">
-              Retirar dinero
-            </Button>
-          </div>
-        </>
-      ) : (
-        <BigInfoCard
-          buttonText="Invertir"
-          icon={<img src="assets/icono-inversor.png" />}
-          to="/investor"
-        >
-          Actualmente no tienes inversiones activas registradas en tu cuenta.
-        </BigInfoCard>
-      )}
-    </div>
+      <InvestModal
+        openModal={openInvestModal}
+        setInvestInfo={setInvestInfo}
+        setOpenModal={setOpenInvestModal}
+      />
+      <RetireMoneyModal
+        openModal={openRetireModal}
+        setInvestInfo={setInvestInfo}
+        setOpenModal={setOpenRetireModal}
+        montoAcumulado={investInfo?.montoActual ?? 0}
+      />
+    </>
   );
 }
 
