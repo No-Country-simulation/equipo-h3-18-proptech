217e2f804ea773897f1b6fbf7241e1c65ae80316
Angel Añez <angdavid2910@gmail.com>
2024-12-16 10:02:34 -0400

    front: style(loader): add classname props to loading page

--- Frontend/h3-18-proptech/src/pages/LoadingPage.tsx
+++ Frontend/h3-18-proptech/src/pages/LoadingPage.tsx
@@ -3,9 +3,10 @@ import { Loader } from "../components/common";
 interface Props {
   background: "tertiary" | "contrast" | "transparent";
   size: "page" | "section";
+  classname?: string;
 }
 
-function LoadingPage({ background, size }: Props) {
+function LoadingPage({ background, size, classname }: Props) {
   const sizeStyle = {
     page: "min-h-screen",
     section: "flex-1",
@@ -19,7 +20,7 @@ function LoadingPage({ background, size }: Props) {
 
   return (
     <section
-      className={`${sizeStyle[size]} ${backgroundStyle[background]}  flex items-center justify-center`}
+      className={`${sizeStyle[size]} ${backgroundStyle[background]} ${classname} flex items-center justify-center`}
     >
       <Loader border="bold" borderColor="primary-blue" size="page" />
     </section>
